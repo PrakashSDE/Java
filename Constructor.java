1ba2b27c8ec430fd590fecaa49e70af13e276c57
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2022-08-18 19:49:20 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Parent
+{
+    public Parent(){
+        System.out.println("Parent Constrcutor");
+    }
+
+     public Parent(int a){
+        System.out.println("Parent Constrcutor 1");
+    }
+
+     public Parent(int c,int d){
+        System.out.println("Parent Constrcutor 2");
+    }
+            
+}
+
+class Child extends Parent
+{
+    public Child(){
+        System.out.println("Child Constructor");
+    }
+
+     public Child(int m){
+        super(m);
+        System.out.println("Child Constructor1");
+    }
+
+     public Child(int n,int k){
+        super(n,k);
+        System.out.println("Child Constructor2");
+    }
+}
+
+
+public class Constructor
+{
+    public static void main(String[] args) {
+      Child ref = new Child(10,20);
+    }  
+}
